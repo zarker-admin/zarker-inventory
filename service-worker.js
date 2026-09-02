413e1f4a1a556acf969dbad2f96b94c2dc24b8f3
zarker-admin <zk1@zarker.co.kr>
2026-09-02 12:39:42 +0900

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,26 @@
-const CACHE='zarker-v1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request).catch(()=>caches.match(e.request).then(r=>r||caches.match('./index.html'))));});
+const CACHE_NAME='zarker-work-v30';
+const APP_SHELL=['./','./index.html','./manifest.webmanifest'];
+
+self.addEventListener('install',event=>{
+ event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(APP_SHELL)).catch(()=>{}));
+ self.skipWaiting();
+});
+
+self.addEventListener('activate',event=>{
+ event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==CACHE_NAME).map(key=>caches.delete(key)))));
+ self.clients.claim();
+});
+
+self.addEventListener('fetch',event=>{
+ if(event.request.method!=='GET')return;
+ if(event.request.mode==='navigate'){
+  event.respondWith(fetch(event.request).then(response=>{
+   const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put('./index.html',copy));return response;
+  }).catch(()=>caches.match('./index.html')));
+  return;
+ }
+ event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+  if(response.ok&&new URL(event.request.url).origin===self.location.origin){const copy=response.clone();caches.open(CACHE_NAME).then(cache=>cache.put(event.request,copy))}
+  return response;
+ })));
+});
